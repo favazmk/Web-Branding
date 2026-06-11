2711d2a734be94375960b3bfd5159858ec5bf668
favazmk12-creator <favazmk12@gmail.com>
2026-06-11 12:54:12 +0530

    Fade out dark overlay on hover while keeping text visible for portfolio and industry cards

---
+++ replace.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let content = fs.readFileSync('campaign.html', 'utf8');
+
+content = content.replace(/style="background-image: linear-gradient\(rgba\(0,0,0,0\.6\), rgba\(0,0,0,0\.8\)\), url\('([^']+)'\); background-size: cover; background-position: center; border: none;"/g, 'style="--bg-image: url(\'$1\');"');
+
+content = content.replace(/style="min-height: 220px; justify-content: flex-end; background-image: linear-gradient\(rgba\(0,0,0,0\.4\), rgba\(0,0,0,0\.8\)\), url\('([^']+)'\); background-size: cover; background-position: center; border: none;"/g, 'style="--bg-image: url(\'$1\');"');
+
+fs.writeFileSync('campaign.html', content);
+console.log('Replaced inline styles');
